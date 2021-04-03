6d00463ee02b625f7c55f77ac409bbf0165d2026
ahmadhuss <ahmadnet600@gmail.com>
2021-04-04 01:16:57 +0500

    refactor: render static categories foreach

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,9 +10,9 @@
 
                 <h1 class="my-4">Shop Name</h1>
                 <div class="list-group">
-                    <a href="#" class="list-group-item">Category 1</a>
-                    <a href="#" class="list-group-item">Category 2</a>
-                    <a href="#" class="list-group-item">Category 3</a>
+                    @foreach($categories as $category)
+                    <a href="#" class="list-group-item">{{ $category }}</a>
+                    @endforeach
                 </div>
 
             </div>
