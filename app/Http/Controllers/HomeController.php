56729c99c1f6daa1971b8db91f4611b6f372ffa9
ahmadhuss <ahmadnet600@gmail.com>
2021-04-04 16:40:22 +0500

    perf: improve Model performance to load relation with() condition inside the Controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,14 @@ class HomeController extends Controller
 
         // This will get all the records from the products table
         // and every record is an object and passed into the view
-        $allProducts = Product::all();
+        // $allProducts = Product::all();
+       // We will increase performance instead of Product::all();
+       // we will use the Product->with('category') It means load all relationship
+        // in the Controller and at final we will use get() instead of all()
+
+
+        // Basically with() is condition and all() is without any condition
+        $allProducts = Product::with('category')->get();
         return view('index', compact('allCategories','allProducts'));
     }
 }
