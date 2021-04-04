1c3f2e9b285eb52a3fd89cefb910f7cce64bd7c0
ahmadhuss <ahmadnet600@gmail.com>
2021-04-04 16:23:23 +0500

    refactor: Added relationship Products table
    * Now we can access the category name with `$product->category->name`

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    // Create relationship of 1 to 1 with Category Model
+    public function Category()
+    {
+        // This help us to do in the view with something like this
+        // index.blade.php => $product->category->name
+        return $this->belongsTo(Category::class);
+    }
+
 }
