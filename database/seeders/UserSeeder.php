227bbd96193f6ca6a969fd57c72d31a7dc763617
ahmadhuss <ahmadnet600@gmail.com>
2021-04-05 14:56:22 +0500

    refactor: `UserSeeder.php` because we want to seed manually with the attributes for User model
    * We are ignoring Model Factories (factories/UserFactory.php)
      and inside this manually specifying the attributes for User model.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +14,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-
+        User::create([
+           'name' => 'Admin',
+           'email' => 'admin@admin.com',
+           'password' => bcrypt('password')// bcrypt `password` & Hash::make('password')
+        ]);
     }
 }
