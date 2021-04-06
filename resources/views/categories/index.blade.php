fd51650eeefa40c92f54dcaf59c6beaa787f777d
ahmadhuss <ahmadnet600@gmail.com>
2021-04-06 13:05:10 +0500

    feat: `Added resources/views/categories/index.blade.php`

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,39 @@
+@extends('app')
+
+@section('content')
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-lg-12">
+
+                <h1 class="my-4">Categories</h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Actions</th>
+                        </tr>
+                        @foreach($categories as $category)
+                            <tr>
+                                <td>{{ $category->name }}</td>
+                                <td>
+                                    <a class="btn btn-primary" href="{{ route('categories.edit', $category->id) }}">Edit</a>
+                                    <a class="btn btn-danger" href="{{ route('categories.destroy', $category->id) }}">Delete</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </thead>
+
+                </table>
+
+            </div>
+            <!-- /.col-lg-12 -->
+
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+@endsection
