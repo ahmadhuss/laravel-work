336c3148b4ac85ee413b1817d18f77991e1e892b
ahmadhuss <ahmadnet600@gmail.com>
2021-04-06 21:26:05 +0500

    refactor: Added body of delete function ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -101,6 +101,8 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $product = Product::findOrFail($id);
+        $product->delete();
+        return redirect()->route('products.index');
     }
 }
