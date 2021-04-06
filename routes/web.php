25a89417dbb038fa507d9eb60bbba2223e477adf
ahmadhuss <ahmadnet600@gmail.com>
2021-04-07 01:13:31 +0500

    feat: used the Route::group function instead of repeating things

--- routes/web.php
+++ routes/web.php
@@ -33,9 +33,13 @@ Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
-// Categories
-Route::resource('/categories', CategoryController::class)->middleware('auth');
-Route::resource('/products', ProductController::class)->middleware('auth');
+// We're repeating things. If there are same middleware and same logic we can group that.
+// Route::resource('/categories', CategoryController::class)->middleware('auth');
+// Route::resource('/products', ProductController::class)->middleware('auth');
+Route::group(['middleware' => 'auth'], function(){
+    Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
+});
 
 
 
