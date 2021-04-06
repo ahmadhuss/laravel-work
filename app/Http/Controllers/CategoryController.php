2a7c060da07c24e21195fa3145703461be13577a
ahmadhuss <ahmadnet600@gmail.com>
2021-04-06 15:55:06 +0500

    feat: Added destroy method function body

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -89,6 +89,8 @@ class CategoryController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $category = Category::find($id);
+        $category->delete();
+        return redirect()->route('categories.index');
     }
 }
