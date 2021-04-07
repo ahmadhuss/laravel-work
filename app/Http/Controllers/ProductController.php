91838075382f7fe69acd2fdb8a5ce7fda24442fc
ahmadhuss <ahmadnet600@gmail.com>
2021-04-07 13:24:41 +0500

    refactor: Store uploaded photo in `storage/app/public/photos` directory
    * Image saved in the database with the following path `photos/${imagePath}.jpg`
    
    * Symlink `storage/app/public/photos` directory to `/public/storage`
    
    * Publicly we can access photos with following URL `asset("storage/$product->photo")`

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -39,12 +39,40 @@ class ProductController extends Controller
      */
     public function store(StoreProductRequest $request)
     {
+        // Q: What does it mean?
+        /**
+         * We will got photo with random name `photo/34432g23h4j32hj4.png`
+         * It was uploaded in `storage/app/photo` (We have a new storage called photo Laravel created!)
+         * So After that, We can store the database filename with directory in the database.
+         * Actual filename was in the `storage` folder.
+         * These are the default settings and these files are not publicly accessible.
+         * But you can access the image with URL something like this:
+         * basic.test/photos/435345345.png
+         * Error: It will not found.
+         * Why?
+         * The `config/filesystems.php` has default array "disks" 'local' &
+         * 'public'. Inside the store('photos' , OnWhichDiskYourDirectoryCreated)
+         * Now Second parameter is important and we enter "public".
+         * Now our storage will be inside "storage/public/photos" directory.
+         * ----------
+         * But Here is the problem In Real world laravel only show assets from the root
+         * "/public" directory and our storage means photos are located at the "/storage/public"
+         * So we have to use symlink or shortcut strategy.
+         * If you symlink storage/app/public, all containing folders will be served there
+         * the only thing that changes is that the content itself is not actually within
+         * public, it is just referenced there.
+         * "php artisan storage:link"
+         * It will create the symlink.
+         */
+        $path = $request->file('photo')->store('photos', 'public');
+        //dd($path); //dump&die
+
         Product::create([
             'name' => $request->name,
             'price' => $request->price,
             'category_id' => $request->category_id,
             'description' => $request->description,
-            'photo' => ''
+            'photo' => $path
         ]);
         return redirect()->route('products.index');
     }
