97f8f848739cf3de9c037db7c6228c15e938ba71
ahmadhuss <ahmadnet600@gmail.com>
2021-04-14 22:23:02 +0500

    feat: Added 2 more ways to create foreign key

--- database/migrations/2021_04_04_084640_create_products_table.php
+++ database/migrations/2021_04_04_084640_create_products_table.php
@@ -19,6 +19,7 @@ class CreateProductsTable extends Migration
             $table->decimal('price');
             $table->string('photo');
 
+            /// 1:
             // Define Relationship
             // To create foreign key in the migration we need to perform 2 line steps
             // It is basically 1 to 1 relationship
@@ -26,6 +27,23 @@ class CreateProductsTable extends Migration
             // Here "on('categories')" represents the "categories" table
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')->references('id')->on('categories');
+
+            // TODO 2:
+            // Foreign column creation alternative way:
+            // It means relate category to product so category_id will be foreign key
+           // $table->integer('category_id')->unsigned()->index(); // Index() helps to speed the database index
+
+            // TODO 3:
+            // -----------------------------
+            // category_id references "categories" table
+            // and the id column Basically this line is telling us. $table->foreignId('category_id')
+            // and constrained() means we have a "foreign key" as a constrained()
+
+            // also we can add onDelete('cascade') It means when when category is deleted the product is
+            // also deleted.
+            // -----------------------------
+          //  $table->foreignId('category_id')->constrained()->onDelete('cascade');
+
 
             $table->text('description');
             $table->timestamps();
